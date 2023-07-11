e995e623256b07a168c98d40c2e6c73c68c67487
Darlan Martins <122477884+Darlan0307@users.noreply.github.com>
2023-07-11 20:51:59 -0300

    Bug no botão

--- Lâmpada/lampada.js
+++ Lâmpada/lampada.js
@@ -1,29 +1,56 @@
-const ligar = document.querySelector('#ligar')
-const desligar = document.querySelector('#desligar')
 const lamp = document.querySelector('#lamp')
 
+const botao = document.querySelector('#botao')
+
+
+
 function isBroken(){
-    return lamp.src.indexOf('quebrada') > - 1
+    return lamp.src.indexOf('quebrada') > -1
+}
+
+function isOff(){
+    return lamp.src.indexOf('desligada') > -1
 }
 
 function ligada(){
-    if(!isBroken()){
-        lamp.src = 'img/ligada.jpg'
+    if (!isBroken()) {
+        if (isOff()) {
+            lamp.src = 'img/ligada.jpg';
+            
+        } else {
+            lamp.src = 'img/desligada.jpg';
+            
+        }
     }
+    atualizarBotao();
 }
 
-function desligada(){
+function desligada() {
     if(!isBroken()){
         lamp.src = 'img/desligada.jpg'
     }
+    atualizarBotao();
 }
 
-function quebrada (){
+
+function quebrado(){
     lamp.src = 'img/quebrada.jpg'
+    botao.textContent = 'Quebrada'
 }
 
-ligar.addEventListener('click',ligada)
-desligar.addEventListener('click',desligada)
+function atualizarBotao() {
+    if (lamp.src.indexOf('desligar') > -1) {
+        botao.textContent = 'Desligar';
+    } else {
+        botao.textContent = 'Ligar';
+    }
+}
+
+atualizarBotao();
+
+
+
+botao.addEventListener('click',ligada)
 lamp.addEventListener('mouseover',ligada)
 lamp.addEventListener('mouseout',desligada)
-lamp.addEventListener('dblclick',quebrada)
+lamp.addEventListener('dblclick',quebrado)
