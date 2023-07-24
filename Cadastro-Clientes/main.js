cb32605269780fe5c105d1b4ac019fe212c25e8d
Darlan Martins <122477884+Darlan0307@users.noreply.github.com>
2023-07-24 19:34:31 -0300

    bug resolvido

--- Cadastro-Clientes/main.js
+++ Cadastro-Clientes/main.js
@@ -5,17 +5,21 @@ const openModal = () => document.getElementById('modal')
 
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
-    
+
 
 const tempCliente = {
-    nome: 'darlan',
+    nome: 'pedro',
     email:'darlanChagas@gmail.com',
     celular:'8789454',
     cidade: 'Jaboatao'
 }
 
 //CRUD - Create Read Update Delete
-
+const createClient = (client) => {
+    const db_clients = JSON.parse(localStorage.getItem('db_client')) ;
+    db_clients.push(client);
+    localStorage.setItem("db_client",JSON.stringify(db_clients));
+}
 
 
 //Eventos
